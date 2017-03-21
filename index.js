4450d3f07cc384c7d93dfa6e964882ed40dfde43
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-21 16:14:40 -0400

    api routes added for index and show

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const app = express();
+const http = require('http').Server(app)
 const parser = require('body-parser');
 const NoteBook = require('./db/models.js').NoteBook
 
 
-const app = express();
-
 app.set('port', process.env.PORT || 3001)
 
+
 app.use('/assets', express.static('public'))
-app.use(parser.json({extend: true}))
+app.use(parser.urlencoded({extend: true}))
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
+app.get('/api/notebooks', function(req, res){
+  Notebook.find({}).then(function(notebooks){
+    res.json(notebooks);
+  });
+})
 
-
+app.get('/api/notebooks/:title', function(req, res){
+  Notebook.findOne({title: req.params.title}).then(function(notebook){
+    res.json(notebook)
+  })
+})
 
 
 app.listen(3001, () => {
