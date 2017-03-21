1333d4ed4fbaefbe640b37028f8458818308c7a7
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-21 14:15:44 -0400

    model created

---
+++ db/models.js
@@ -0,0 +1,14 @@
+const mongoose = require('./connection.js')
+
+const NoteBookSchema = new mongoose.Schema({
+  title: String,
+  entry: String
+}, {
+  timestamps: true
+})
+
+const NoteBook = mongoose.model('NoteBook', NoteBookSchema)
+
+module.exports = {
+  Notebook: Notebook
+}
